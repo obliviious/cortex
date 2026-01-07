91229c008acf1e81e4e93c7edd1ed0ad1b4a18e2
Aditya Raj <obliviious@zohomail.in>
2026-01-07 16:55:20 +0530

    added vercel analytics

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -1,16 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react'
 import Landing from './pages/Landing'
 import Docs from './pages/Docs'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Landing />} />
-        <Route path="/docs" element={<Docs />} />
-        <Route path="/docs/:section" element={<Docs />} />
-      </Routes>
-    </Router>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/docs" element={<Docs />} />
+          <Route path="/docs/:section" element={<Docs />} />
+        </Routes>
+      </Router>
+      <Analytics />
+    </>
   )
 }
 
